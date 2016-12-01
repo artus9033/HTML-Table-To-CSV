add97f1809ada786aadc070c0be0e084af9f0db9
artus9033@gmail.com <artus9033@gmail.com>
2016-12-01 18:10:24 +0100

    Fix replacing characters

--- JHT-CSV-Worker.js
+++ JHT-CSV-Worker.js
@@ -8,6 +8,10 @@ function checkColumn(index, arr){
 	return chk;
 }
 
+function escapeRegExp(str) {
+    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
+
 function replaceAll(str, find, replace) {
   return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
 }
