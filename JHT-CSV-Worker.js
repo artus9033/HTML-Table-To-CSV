864746ee01d047cb41881d407c0c2a68b20d1b70
artus9033@gmail.com <artus9033@gmail.com>
2016-12-02 16:12:23 +0100

    Added <thead> support

--- JHT-CSV-Worker.js
+++ JHT-CSV-Worker.js
@@ -9,11 +9,11 @@ function checkColumn(index, arr){
 }
 
 function escapeRegExp(str) {
-    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+	return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
 }
 
 function replaceAll(str, find, replace) {
-  return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+	return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
 }
 
 function JHTCSVWorker(){
@@ -21,12 +21,13 @@ function JHTCSVWorker(){
 	console.log("Repository on GitHub: https://github.com/artus9033/JQuery-HTML-Table-CSV-Export-Import");
 }
 
-JHTCSVWorker.prototype.exportToCSV = function(splicePos, spliceLength, selector, excludedColumns, lastCol, minusSubstitute){
-		var arr = new Array();
-		var mCt = 0;
-		var klucz = 0;
-		var delimiterKolumn = ",";
-		$(selector + ' tbody').children().each(function(){
+JHTCSVWorker.prototype.exportToCSV = function(splicePos, spliceLength, selector, excludedColumns, lastCol, minusSubstitute, hasHeader){
+	var arr = new Array();
+	var mCt = 0;
+	var klucz = 0;
+	var delimiterKolumn = ",";
+	if(hasHeader == true){
+		$(selector + ' thead').children().each(function(){
 			$(this).children().each(function(index, elem){
 				mCt++;
 				if(checkColumn(mCt, excludedColumns)){
@@ -39,139 +40,153 @@ JHTCSVWorker.prototype.exportToCSV = function(splicePos, spliceLength, selector,
 				}
 			});
 		});
-		arr.splice(splicePos, spliceLength);
-		for(var i=0; i < arr.length; i++) {
-			arr[i] = replaceAll(arr[i], 'undefined', ''); //removes 'undefined'
-			arr[i] = replaceAll(arr[i], '-', minusSubstitute); //replaces '-' with minusSubstitute
+	}
+$(selector + ' tbody').children().each(function(){
+	$(this).children().each(function(index, elem){
+		mCt++;
+		if(checkColumn(mCt, excludedColumns)){
+			arr[klucz] += $(this).text();
+			arr[klucz] += delimiterKolumn;
 		}
-		var csv;
-		$.each(arr, function(index, value) {
-			csv += value + "\r\n";
-		});
-		console.log("Succesfully exported table " + selector + "! The raw csv is:")
-		console.log(arr);
-		csv = csv.substring(9);
-		var encoder = new DataEnc({
-		    mime   : 'text/csv',
-		    charset: 'UTF-16BE',
-		    bom    : true
-		});
-		encoder.enc(csv);
-		var result = encoder.pay();
-		return result;
+		if(mCt == lastCol){
+			mCt = 0;
+			klucz++;
+		}
+	});
+});
+arr.splice(splicePos, spliceLength);
+for(var i=0; i < arr.length; i++) {
+	arr[i] = replaceAll(arr[i], 'undefined', ''); //removes 'undefined'
+	arr[i] = replaceAll(arr[i], '-', minusSubstitute); //replaces '-' with minusSubstitute
+}
+var csv;
+$.each(arr, function(index, value) {
+	csv += value + "\r\n";
+});
+console.log("Succesfully exported table " + selector + "! The raw csv is:")
+console.log(arr);
+csv = csv.substring(9);
+var encoder = new DataEnc({
+	mime   : 'text/csv',
+	charset: 'UTF-16BE',
+	bom    : true
+});
+encoder.enc(csv);
+var result = encoder.pay();
+return result;
 };
 
 /**
- * DataEnc definition
- */
+* DataEnc definition
+*/
 
 function DataEnc(a) {
-    this.config(a);
+	this.config(a);
 }
 DataEnc._enctype = {
-        u8    : ['u8', 'utf8'],
-        u16be : ['u16', 'u16be', 'utf16', 'utf16be', 'ucs2', 'ucs2be'],
-        u16le : ['u16le', 'utf16le', 'ucs2le']
+	u8    : ['u8', 'utf8'],
+	u16be : ['u16', 'u16be', 'utf16', 'utf16be', 'ucs2', 'ucs2be'],
+	u16le : ['u16le', 'utf16le', 'ucs2le']
 };
 DataEnc._BOM = {
-        'none'     : '',
-        'UTF-8'    : '%ef%bb%bf',
-        'UTF-16BE' : '%fe%ff',
-        'UTF-16LE' : '%ff%fe'
+	'none'     : '',
+	'UTF-8'    : '%ef%bb%bf',
+	'UTF-16BE' : '%fe%ff',
+	'UTF-16LE' : '%ff%fe'
 };
 DataEnc.prototype = {
-    config : function(a) {
-        var opt = {
-            charset: 'u8',
-            mime   : 'text/csv',
-            base64 : 0,
-            bom    : 0
-        }, p;
-        a = a || {};
-        for (p in opt) {
-            if (opt.hasOwnProperty(p))
-                this[p] = a.hasOwnProperty(p) ? a[p] : opt[p];
-        }
-        this.buf  = '';
-        this.lead = '';
-        this.__intro();
-        return this;
-    },
-    __intro : function() {
-        var
-            g = [],
-            c = this.charset || '',
-            b = 'none'
-        ;
-        if (this.mime && this.mime !== '')
-            g.push(this.mime);
-        if (c !== '') {
-            c = c.replace(/[-\s]/g, '').toLowerCase();
-            if (DataEnc._enctype.u8.indexOf(c) > -1) {
-                c = 'UTF-8';
-                if (this.bom)
-                    b = c;
-                this.enc = this.utf8;
-            } else if (DataEnc._enctype.u16be.indexOf(c) > -1) {
-                c = 'UTF-16BE';
-                if (this.bom)
-                    b = c;
-                this.enc = this.utf16be;
-            } else if (DataEnc._enctype.u16le.indexOf(c) > -1) {
-                c = 'UTF-16LE';
-                if (this.bom)
-                    b = c;
-                this.enc = this.utf16le;
-            } else {
-                if (c === 'copy')
-                    c = '';
-                this.enc = this.copy;
-            }
-        }
-        if (c !== '')
-            g.push('charset=' + c);
-        if (this.base64)
-            g.push('base64');
-        this.lead = 'data:' + g.join(';') + ',' + DataEnc._BOM[b];
-        return this;
-    },
-    pay : function() {
-        return this.lead + this.buf;
-    },
-    utf16be : function(t) {
-        var i, c, buf = [];
-        for (i = 0; i < t.length; ++i) {
-            if ((c = t.charCodeAt(i)) > 0xff) {
-                buf.push(('0' + (c >> 0x08).toString(16)).substr(-2));
-                buf.push(('0' + (c  & 0xff).toString(16)).substr(-2));
-            } else {
-                buf.push('00');
-                buf.push(('0' + (c  & 0xff).toString(16)).substr(-2));
-            }
-        }
-        this.buf += '%' + buf.join('%');
-        return buf;
-    },
-    utf16le : function(t) {
-        var i, c, buf = [];
-        for (i = 0; i < t.length; ++i) {
-            if ((c = t.charCodeAt(i)) > 0xff) {
-                buf.push(('0' + (c  & 0xff).toString(16)).substr(-2));
-                buf.push(('0' + (c >> 0x08).toString(16)).substr(-2));
-            } else {
-                buf.push(('0' + (c  & 0xff).toString(16)).substr(-2));
-                buf.push('00');
-            }
-        }
-        this.buf += '%' + buf.join('%');
-        return buf;
-    },
-    utf8 : function(t) {
-        this.buf += encodeURIComponent(t);
-        return this;
-    },
-    copy : function(t) {
-        this.buf += t;
-        return this;
-    }
+	config : function(a) {
+		var opt = {
+			charset: 'u8',
+			mime   : 'text/csv',
+			base64 : 0,
+			bom    : 0
+		}, p;
+		a = a || {};
+		for (p in opt) {
+			if (opt.hasOwnProperty(p))
+			this[p] = a.hasOwnProperty(p) ? a[p] : opt[p];
+		}
+		this.buf  = '';
+		this.lead = '';
+		this.__intro();
+		return this;
+	},
+	__intro : function() {
+		var
+		g = [],
+		c = this.charset || '',
+		b = 'none'
+		;
+		if (this.mime && this.mime !== '')
+		g.push(this.mime);
+		if (c !== '') {
+			c = c.replace(/[-\s]/g, '').toLowerCase();
+			if (DataEnc._enctype.u8.indexOf(c) > -1) {
+				c = 'UTF-8';
+				if (this.bom)
+				b = c;
+				this.enc = this.utf8;
+			} else if (DataEnc._enctype.u16be.indexOf(c) > -1) {
+				c = 'UTF-16BE';
+				if (this.bom)
+				b = c;
+				this.enc = this.utf16be;
+			} else if (DataEnc._enctype.u16le.indexOf(c) > -1) {
+				c = 'UTF-16LE';
+				if (this.bom)
+				b = c;
+				this.enc = this.utf16le;
+			} else {
+				if (c === 'copy')
+				c = '';
+				this.enc = this.copy;
+			}
+		}
+		if (c !== '')
+		g.push('charset=' + c);
+		if (this.base64)
+		g.push('base64');
+		this.lead = 'data:' + g.join(';') + ',' + DataEnc._BOM[b];
+		return this;
+	},
+	pay : function() {
+		return this.lead + this.buf;
+	},
+	utf16be : function(t) {
+		var i, c, buf = [];
+		for (i = 0; i < t.length; ++i) {
+			if ((c = t.charCodeAt(i)) > 0xff) {
+				buf.push(('0' + (c >> 0x08).toString(16)).substr(-2));
+				buf.push(('0' + (c  & 0xff).toString(16)).substr(-2));
+			} else {
+				buf.push('00');
+				buf.push(('0' + (c  & 0xff).toString(16)).substr(-2));
+			}
+		}
+		this.buf += '%' + buf.join('%');
+		return buf;
+	},
+	utf16le : function(t) {
+		var i, c, buf = [];
+		for (i = 0; i < t.length; ++i) {
+			if ((c = t.charCodeAt(i)) > 0xff) {
+				buf.push(('0' + (c  & 0xff).toString(16)).substr(-2));
+				buf.push(('0' + (c >> 0x08).toString(16)).substr(-2));
+			} else {
+				buf.push(('0' + (c  & 0xff).toString(16)).substr(-2));
+				buf.push('00');
+			}
+		}
+		this.buf += '%' + buf.join('%');
+		return buf;
+	},
+	utf8 : function(t) {
+		this.buf += encodeURIComponent(t);
+		return this;
+	},
+	copy : function(t) {
+		this.buf += t;
+		return this;
+	}
 };
--- README.md
+++ README.md
@@ -26,10 +26,10 @@ To use it simply init a new instance of JHTCSVWorker:
 initializes a new instance of JHTCSVWorker
 
 ```javascript
-(string) JHTCSVWorker.exportToCSV(splicePos (int / int[]), spliceLength (int / int[]), selector (string), excludedColumns (int[]), lastCol (int), minusSubstitute (string);
+(string) JHTCSVWorker.exportToCSV(splicePos (int / int[]), spliceLength (int / int[]), selector (string), excludedColumns (int[]), lastCol (int), minusSubstitute (string), hasHeader (boolean);
 ```
 
-Returns the string with data exported from a table specified by the **selector**, which is a selector for JQuery (e.g. "#id" or ".class"), also removing rows specified by **splicePos** and **spliceLength** (both int or both int[]), which are as follows: the first number or array of numbers for multiple rows to be excluded from export and the corresponding last number / array of numbers to be excluded (same as the Javascript's `string.splice(pos, length)` function). If **splicePos** is negative, the function counts from the last row to the first. The **excludedColumns** is an array of int, which contains numbers of columns (starting from 1) to be excluded from exporting. The **lastCol** is an int which is the total number of columns in the table. The **minusSubstitute** is the text to be placed instead of '-' characters which create coflicts with OpenOffice.
+Returns the string with data exported from a table specified by the **selector**, which is a selector for JQuery (e.g. "#id" or ".class"), also removing rows specified by **splicePos** and **spliceLength** (both int or both int[]), which are as follows: the first number or array of numbers for multiple rows to be excluded from export and the corresponding last number / array of numbers to be excluded (same as the Javascript's `string.splice(pos, length)` function). If **splicePos** is negative, the function counts from the last row to the first. The **excludedColumns** is an array of int, which contains numbers of columns (starting from 1) to be excluded from exporting. The **lastCol** is an int which is the total number of columns in the table. The **minusSubstitute** is the text to be placed instead of '-' characters which create coflicts with OpenOffice. The **hasHeader** is a boolean which, if it's true, indicates that the <table> has a <thead> header that is meant to be attached to the csv as columns header, or, if false, makes the script not to attach the <thead> to the exported csv.
 
 # Sample code:
 ***
